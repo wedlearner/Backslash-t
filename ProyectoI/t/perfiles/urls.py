e2602e1e7882b2848f2516a0885482b0460de7ae
mlopeznSS <maximilianolopeznun@gmail.com>
2025-10-29 11:26:06 -0300

    Add files via upload

--- ProyectoI/t/perfiles/urls.py
+++ ProyectoI/t/perfiles/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('perfiles/', views.ver_perfil, name = 'ver_perfil')
+    path('ver_perfil', views.ver_perfil, name = 'ver_perfil'),
+    path('editar_perfil/',views.editar_perfil, name = 'editar_perfil')
 ]
