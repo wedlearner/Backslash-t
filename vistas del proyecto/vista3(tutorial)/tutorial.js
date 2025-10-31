c4d9cf0016c2b7336d16539522d9f2964cced798
moncra <jss.zcng@gmail.com>
2025-10-31 12:31:46 -0300

    Delete vistas del proyecto directory

--- vistas del proyecto/vista3(tutorial)/tutorial.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('.desplegable').forEach(boton => {
-    boton.addEventListener('click', () => {
-        const submenu = boton.nextElementSibling;
-        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-    });
-});
