f94120baf8713f84505efb1d46694cc4f164f2ab
moncra <jss.zcng@gmail.com>
2025-10-31 12:41:08 -0300

    Delete ProyectoI/t directory

--- ProyectoI/t/perfiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('ver_perfil', views.ver_perfil, name = 'ver_perfil'),
-    path('editar_perfil/',views.editar_perfil, name = 'editar_perfil')
-]
