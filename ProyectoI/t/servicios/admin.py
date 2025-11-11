6f4e19da6146dd1141c40cd085a1fbd7abb391be
mlopeznSS <maximilianolopeznun@gmail.com>
2025-11-11 15:21:16 -0300

    Delete ProyectoI/t directory

--- ProyectoI/t/servicios/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import servicio
-admin.site.register(servicio)
-# Register your models here.
--- ProyectoI/t/servicios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-app_name = 'servicios'
-
-urlpatterns = [
-    path('', views.ver_servicios, name = 'ver_servicios')
-
-]
